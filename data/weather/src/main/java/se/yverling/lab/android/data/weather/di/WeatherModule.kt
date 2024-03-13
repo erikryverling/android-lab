184829b2eff3eafce9207067b27cccbd9eadc264
Erik R. Yverling <erik.r@yverling.se>
2024-03-13 23:10:33 +0100

    Use kotlinx-serialization instead of Moshi with Retrofit

--- data/weather/src/main/java/se/yverling/lab/android/data/weather/di/WeatherModule.kt
+++ data/weather/src/main/java/se/yverling/lab/android/data/weather/di/WeatherModule.kt
@@ -1,5 +1,6 @@
 package se.yverling.lab.android.data.weather.di
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -10,9 +11,11 @@ import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import retrofit2.Converter
 import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 import se.yverling.lab.android.data.weather.network.WeatherService
+import javax.inject.Named
 import javax.inject.Singleton
 
 private const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
@@ -20,12 +23,32 @@ private const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
 @Module
 @InstallIn(SingletonComponent::class)
 class WeatherModule {
+    @Provides
+    @Singleton
+    @Named("kotlinSerializationJson")
+    fun providesKotlinSerializationJson(): Json = Json {
+        ignoreUnknownKeys = true
+        prettyPrint = true
+    }
+
+    @Provides
+    @Singleton
+    @Named("kotlinSerialization")
+    fun providesKotlinSerializationJsonConverterFactory(
+        @Named("kotlinSerializationJson") json: Json,
+    ): Converter.Factory {
+        val contentType = MediaType.get("application/json")
+        return json.asConverterFactory(contentType)
+    }
+
     @Singleton
     @Provides
-    fun provideRetrofit(): Retrofit {
+    fun provideRetrofit(
+        @Named("kotlinSerialization") kotlinSerializationJsonConverterFactory: Converter.Factory
+    ): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(MoshiConverterFactory.create())
+            .addConverterFactory(kotlinSerializationJsonConverterFactory)
             .build()
     }
 
