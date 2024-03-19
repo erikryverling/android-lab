dc400dfc85c264ccda3a4a2741eea7085a982158
Erik R. Yverling <erik.r@yverling.se>
2024-03-19 12:57:30 +0100

    Add support for Compose metrics reports

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.util.*
+
 allprojects {
     repositories {
         google()
@@ -6,7 +9,7 @@ allprojects {
 }
 
 plugins {
-    alias(libs.plugins.android.application) apply false 
+    alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.test) apply false
     alias(libs.plugins.android.kotlin) apply false
     alias(libs.plugins.hilt) apply false
@@ -16,6 +19,7 @@ plugins {
     alias(libs.plugins.serialization) apply false
 }
 
+// Run with ./gradlew dependencyUpdates
 tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
     rejectVersionIf {
         isNonStable(candidate.version)
@@ -23,8 +27,30 @@ tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 }
 
 fun isNonStable(version: String): Boolean {
-    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase(Locale.getDefault()).contains(it) }
     val regex = "^[0-9,.v-]+(-r)?$".toRegex()
     val isStable = stableKeyword || regex.matches(version)
     return isStable.not()
+}
+
+// Run with /gradlew assembleDebug -PandroidLab.enableComposeCompilerReports=true
+// See: https://chrisbanes.me/posts/composable-metrics for more information
+subprojects {
+    tasks.withType(KotlinCompile::class.java).configureEach {
+        kotlinOptions {
+            if (project.findProperty("androidLab.enableComposeCompilerReports") == "true") {
+                freeCompilerArgs = freeCompilerArgs + listOf(
+                    "-P",
+                    "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=" +
+                            layout.buildDirectory.get() + "/compose_metrics"
+                )
+
+                freeCompilerArgs = freeCompilerArgs + listOf(
+                    "-P",
+                    "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=" +
+                            layout.buildDirectory.get() + "/compose_metrics"
+                )
+            }
+        }
+    }
 }
