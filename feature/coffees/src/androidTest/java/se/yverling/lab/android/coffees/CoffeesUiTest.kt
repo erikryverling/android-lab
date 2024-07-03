a9ff4140a045a48b03cd71178e6bc54b4ab09cc6
Erik R. Yverling <erik.r@yverling.se>
2024-07-03 16:56:38 +0200

    Fix the rotation on Coffee screen

--- feature/coffees/src/androidTest/java/se/yverling/lab/android/coffees/CoffeesUiTest.kt
+++ feature/coffees/src/androidTest/java/se/yverling/lab/android/coffees/CoffeesUiTest.kt
@@ -1,32 +1,43 @@
 package se.yverling.lab.android.coffees
 
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onAllNodesWithText
-import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.performClick
-import dagger.hilt.android.testing.HiltAndroidRule
-import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Rule
 import org.junit.Test
+import se.yverling.lab.android.coffees.model.Coffee
+import se.yverling.lab.android.coffees.ui.CoffeeList
+import se.yverling.lab.android.design.theme.AndroidLabTheme
 
-@HiltAndroidTest
 class CoffeesUiTest {
-    @get:Rule(order = 0)
-    var hiltRule = HiltAndroidRule(this)
-
-    @get:Rule(order = 1)
-    val composeTestRule = createAndroidComposeRule(CoffeesTestActivity::class.java)
+    @get:Rule
+    val composeTestRule = createComposeRule()
 
     @Test
     fun should_tap_on_FAB_button_successfully() {
-        val coffeeName = "Odo Carbonic"
+        composeTestRule.setContent {
+            AndroidLabTheme {
+                val coffees = listOf(
+                    Coffee(
+                        id = 0,
+                        name = "name",
+                        roaster = "roaster",
+                        origin = "origin",
+                        region = "region"
+                    )
+                )
+
+                CoffeeList(
+                    coffees = coffees,
+                    isUppercase = false,
+                    showHeading = false
+                ) {
+
+                }
+            }
+        }
+
+        val coffeeName = "name"
 
         composeTestRule.onAllNodesWithText(coffeeName)[0].assertExists()
-        composeTestRule.onAllNodesWithText(coffeeName.uppercase())[0].assertDoesNotExist()
-
-        composeTestRule.onNodeWithTag("FAB").performClick()
-
-        composeTestRule.onAllNodesWithText(coffeeName)[0].assertDoesNotExist()
-        composeTestRule.onAllNodesWithText(coffeeName.uppercase())[0].assertExists()
     }
 }
