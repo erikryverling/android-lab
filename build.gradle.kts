11c722c188adc9176a54b1323d24c97632492cf9
Erik R. Yverling <erik.r@yverling.se>
2024-12-02 20:21:01 +0100

    Remove root build.gradle.kts

--- build.gradle.kts
+++
@@ -1,6 +0,0 @@
-plugins {
-    // NOTE! There are more plugins defined in buildSrc/build.gradle.kts
-    alias(libs.plugins.protobuf) apply false
-    alias(libs.plugins.paparazzi) apply false
-    alias(libs.plugins.serialization) apply false
-}
