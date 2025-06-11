d18dbe74ac1b10c52447fd7e8d83c5eb4c146ffb
Erik R. Yverling <erik.r@yverling.se>
2025-06-11 17:16:19 +0200

    Replace CircularProgressIndicator with LoadingIndicator

--- common/ui/src/main/kotlin/se/yverling/lab/android/ui/LoadingScreen.kt
+++ common/ui/src/main/kotlin/se/yverling/lab/android/ui/LoadingScreen.kt
@@ -2,17 +2,19 @@ package se.yverling.lab.android.ui
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.LoadingIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun LoadingScreen(modifier: Modifier = Modifier) {
     Box(
         modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        CircularProgressIndicator()
+        LoadingIndicator()
     }
 }
